9495ef2e41ff39e620630b36f43c4f222d5d774d
cmparlettpelleriti <parlett@chapman.edu>
2017-09-27 04:59:49 +0000

    all the files

--- cplane.py
+++ cplane.py
@@ -1,69 +1,101 @@
 import abscplane
+from abc import ABC, abstractmethod
 
-class ListComplexPlane(AbsComplexPlane):
+class ListComplexPlane(abscplane.AbsComplexPlane):
     """complex planes"""
+#     xmin = 0
+#     xmax  = 0
+#     xlen  = 0
+#     ymin  = 0
+#     ymax  = 0
+#     ylen  = 0
+#     plane = []
+#     fs = []
     
     def __init__(self,xmin,xmax,xlen,ymin,ymax,ylen):
-        xmin  = float(xmin)
-        xmax  = float(xmax)
-        xlen  = float(xlen)
-        ymin  = float(ymin)
-        ymax  = float(ymax)
-        ylen  = float(ylen)
-        plane =  []
-        fs = []
+        self.xmin  = float(xmin)
+        self.xmax  = float(xmax)
+        self.xlen  = int(xlen)
+        self.ymin  = float(ymin)
+        self.ymax  = float(ymax)
+        self.ylen  = int(ylen)
+        self.plane =  []
+        self.fs = []
         # The implementation type of plane is up to the user
         # fs should be a list of functions, initialized to be empty
-        for f in fs:
-            fs.append()
         #x's
-        dx = (xmax-xmin)/xlen
-        xs = [xmin]
-        for i in range(0,xlen):
-            s = xmin
-            s += dx #increment based on length of dx
+        self.dx = (self.xmax-self.xmin)/(self.xlen-1)
+        xs = [self.xmin]
+        s = self.xmin
+        for i in range(0,self.xlen-1):
+            s += self.dx #increment based on length of dx
+            s = round(s,3)
             xs.append(s)
-        dy = (ymax-ymin)/ylen
+        self.dy = (self.ymax-self.ymin)/(self.ylen-1)
         yx = []
-        for i in range(0,xlen):
-            s = ymin
-            s += dy #increment based on length of dx
+        ys = [self.ymin*1j]
+        s = self.ymin
+        for i in range(0,self.ylen-1):
+            s += self.dy #increment based on length of dx
+            s = round(s,3)
             ys.append(s*1j)
         
-        for i in len(xs):
-            plane.append(xs[i]+ys[i])
+        for i in range(0,self.xlen):
+            p = []
+            for k in range(0,self.ylen):
+                p.append(xs[i]+ys[k])
+            self.plane.append(p)
+        print(self.plane)
         
-        return plane
     def apply(self,f):
-        fs.append(f)
-        for i in plane:
-            plane[i] = map(f,plane[i])
-        return plane
+        self.fs.append(f)
+        print(self.plane)
+        for i in range(0,len(self.plane)):
+            for k in range(0,len(self.plane[i])):
+                self.plane[i][k] = f(self.plane[i][k])
+        print(self.plane)
+    
     def refresh(self):
+        self.xmin  = float(xmin)
+        self.xmax  = float(xmax)
+        self.xlen  = int(xlen)
+        self.ymin  = float(ymin)
+        self.ymax  = float(ymax)
+        self.ylen  = int(ylen)
+        self.plane =  []
+        self.fs = []
+        # The implementation type of plane is up to the user
+        # fs should be a list of functions, initialized to be empty
         #x's
-        dx = (xmax-xmin)/xlen
-        xs = [xmin]
-        for i in range(0,xlen):
-            s = xmin
-            s += dx #increment based on length of dx
+        self.dx = (self.xmax-self.xmin)/(self.xlen-1)
+        xs = [self.xmin]
+        s = self.xmin
+        for i in range(0,self.xlen-1):
+            s += self.dx #increment based on length of dx
+            s = round(s,3)
             xs.append(s)
-        dy = (ymax-ymin)/ylen
+        self.dy = (self.ymax-self.ymin)/(self.ylen-1)
         yx = []
-        for i in range(0,xlen):
-            s = ymin
-            s += dy #increment based on length of dx
+        ys = [self.ymin*1j]
+        s = self.ymin
+        for i in range(0,self.ylen-1):
+            s += self.dy #increment based on length of dx
+            s = round(s,3)
             ys.append(s*1j)
         
-        for i in len(xs): #combine x's and y's
-            plane.append(xs[i]+ys[i])
-        fs = []
-        return plane
+        for i in range(0,self.xlen):
+            p = []
+            for k in range(0,self.ylen):
+                p.append(xs[i]+ys[k])
+            self.plane.append(p)
         
     def zoom(self,xmin,xmax,xlen,ymin,ymax,ylen):
-        __init__()
         for f in fs:
             apply(f)
         return plane
+    
+    def print_LIST(self,plane):
+        print(plane)
         
 
            
